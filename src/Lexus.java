38540236277aebcb750806e64eb0aee7b36eab54
KovalskaOlha <unlimolg@gmail.com>
2023-04-11 17:08:47 -0400

    lexus commit

---
+++ src/Lexus.java
@@ -0,0 +1,5 @@
+public class Lexus {
+    String model;
+    int year;
+    double price;
+}
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         audi.model="TT";
         audi.price=65000.0;
         audi.year=2022;
+
+        Lexus lexus=new Lexus();
+        lexus.model="RX400h";
+        lexus.year=2021;
+        lexus.price=35555.0;
+        
         
     }
 }
